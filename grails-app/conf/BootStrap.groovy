a5e8590551d13966504a6a89aa5b0a79c64e4951
Carlus Henry <carlushenry@gmail.com>
2010-12-28 09:03:58 -0500

    making the Bootstrap more groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -13,36 +13,29 @@ class BootStrap {
 
 
     def populateDevData() {
-	Skill java = new Skill(name:"Java", description:"Programming Language")
-	Skill ruby = new Skill(name:"Ruby", description:"Programming Language")
-	Skill rails = new Skill(name:"Rails", description:"Web framework for Ruby")
-	Skill php = new Skill(name:"PHP", description:"Programming Language")
-	Skill groovy = new Skill(name:"Groovy", description:"Programming Language")
-	Skill grails = new Skill(name:"Grails", description:"Web framework for Groovy")
+	def java = new Skill(name:"Java", description:"Programming Language")
+	def ruby = new Skill(name:"Ruby", description:"Programming Language")
+	def rails = new Skill(name:"Rails", description:"Web framework for Ruby")
+	def php = new Skill(name:"PHP", description:"Programming Language")
+	def groovy = new Skill(name:"Groovy", description:"Programming Language")
+	def grails = new Skill(name:"Grails", description:"Web framework for Groovy")
 
-	Person me = new Person(firstName:"Carlus", lastName:"Henry", description:"Me.")	
-	Person atheesh = new Person(firstName:"Atheesh", lastName:"Sanka", description:"Fellow Consultant at GFS for the past 4 years")
-	Person ja = new Person(firstName:"Jeremy", lastName:"Anderson", description:"Pillar Consultant at GFS, wrote Flex on Java for Manning")
+	def me = new Person(firstName:"Carlus", lastName:"Henry", description:"Me.")	
+			.addToSkills(java)
+			.addToSkills(groovy)
+			.addToSkills(grails)
+			.save(flush:true)
 
-	me.addToSkills(java)
-	me.addToSkills(groovy)
-	me.addToSkills(grails)
-
-	atheesh.addToSkills(java)
-
-	ja.addToSkills(java)
-	ja.addToSkills(ruby)
-	ja.addToSkills(rails)
-	ja.addToSkills(groovy)
-	ja.addToSkills(grails)
-
-	def people = [me, atheesh, ja]
-
-
-	people.each {
-	   it.save(flush:true) 
-	}
-	
+	def atheesh = new Person(firstName:"Atheesh", lastName:"Sanka", description:"Fellow Consultant at GFS for the past 4 years")
+			.addToSkills(java)
+			.save(flush:true)
+	def ja = new Person(firstName:"Jeremy", lastName:"Anderson", description:"Pillar Consultant at GFS, wrote Flex on Java for Manning")
+			.addToSkills(java)
+			.addToSkills(ruby)
+			.addToSkills(rails)
+			.addToSkills(groovy)
+			.addToSkills(grails)
+			.save(flush:true)
     }
 
     def destroy = {
